f108f93d1ff41bbf7fa9c26988ded3faf26223a1
amywinehousing <amywinehousing@protonmail.com>
2025-12-14 00:59:51 +0200

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,166 @@
+/*
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡔⢹⠀⠀⠀⠀⠀⠀⠀⢀⠜⡇
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠎⠀⢸⠀⠀⠀⠀⠀⠀⡰⠁⠀⡇
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡴⠃⠀⠀⡸⠀⠀⠀⠀⢀⠜⠁⠀⢀⠇
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡠⠜⠋⠀⠀⠀⢀⠃⠀⣀⠤⠒⠁⠀⠀⠀⡜⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠔⠚⠉⠀⠀⠀⠀⠀⠀⠀⠚⠂⠉⠀⠀⠀⠀⠀⠀⡜⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠔⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠊⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠌⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣃⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠔⠁⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⠜⠬⣀⠀⠈⠢⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⡖⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢀⠔⠁⠀⠀⠀⠑⢤⡀⠈⠢⡀⠀⠀⠀⢀⣠⣴⡾⡯⣴⣀⠤⢤⡤⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⡔⠁⠀⠀⠀⠀⢀⠀⠀⠑⢄⠀⠈⢒⣴⡿⠟⠋⠁⢠⣮⡽⣩⣚⡁⠤⢲⠃⠀⠀⠀⠀⠀
+⠀⠀⢀⠎⠀⠀⠀⢀⣠⣴⠏⠀⠀⠀⢨⢓⡴⠓⠲⡕⡄⢀⡴⡫⠋⠀⠀⠀⠀⠠⠃⠀⠀⠀⠀⠀⠀
+⠀⢠⠂⠀⠀⢀⡾⠟⣉⡴⠞⠋⠁⠀⠸⡸⡀⠀⠀⡸⢘⡭⠊⠀⠀⠀⠀⢀⠔⠁⠀⠀⠀⠀⠀⠀⠀<---- λsriel
+⢀⠂⣀⡀⠀⠘⠀⡞⠁⠀⡄⠀⠀⠀⠀⠑⠬⢒⠮⠔⠁⠀⠀⠀⠀⠠⠔⠥⣤⠄⠀⠀⠀⠀⠀⠀⠀
+⡎⢸⡏⠁⠀⠀⠀⠀⣠⡾⠁⠀⠀⠀⠀⣠⠗⠃⠀⠀⠀⠀⠀⢀⡀⠤⠐⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀
+⠑⢄⣀⣠⣶⣶⠿⠟⠋⠀⠀⢀⣠⠴⠚⠁⠀⠀⢀⠴⠓⠂⢄⡀⠉⠢⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠙⢏⠁⠀⠀⢀⣀⠴⠊⠉⠀⠀⠀⢀⡠⠒⠁⠀⠀⢀⣀⠀⠈⠉⠘⠢⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠑⠒⠉⠁⢸⡄⡀⠀⠀⡠⠔⠉⠀⣀⠤⠐⣈⠥⠒⠈⢁⣐⡦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢠⡇⢪⣥⡖⠉⢀⣠⣴⣿⣥⠒⠉⠀⠀⠒⠋⠉⠀⠀⠁⢢⣄⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠈⢰⡏⡇⢠⠶⢿⡿⠛⠋⠀⠀⠀⠀⠀⣤⠰⠶⠆⠀⠛⠃⠙⠇⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢀⣤⡊⢵⡦⠑⠁⠀⠀⠀⣀⣠⠤⠖⠂⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⡠⠋⣁⣴⣴⠾⠒⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣐⡵⠚⠂⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+				12:59 AM 14.12.2025⠀⠀⠀⠀⠀⠀⠀⠀
+*/
+
+#include<iostream>
+#include<ctime>
+#include<chrono>
+#include<thread>
+using namespace std;
+
+void fillArray(int dealerCard[], int playerCard[], const int POSSIBLECARDS) {
+	for (int i = 0; i < POSSIBLECARDS; i++) {
+		dealerCard[i] = rand() % 9 + 2;
+		playerCard[i] = rand() % 9 + 2;
+	}
+}
+
+int main() {
+    srand(time(NULL));
+    const int POSSIBLECARDS = 21;
+    int dealerCardVal = 0;
+    int playerCardVal = 1;
+    int dealerCard[POSSIBLECARDS]{};
+    int playerCard[POSSIBLECARDS]{};
+    char choice;
+    
+    fillArray(dealerCard, playerCard, POSSIBLECARDS);
+    
+    while (true) {
+        int tempPlayerSum = 0, tempDealerSum = 0;
+        
+        dealerCardVal = 0;
+        playerCardVal = 1;
+        
+        for (int i = 0; i <= dealerCardVal; i++) {
+            tempDealerSum += dealerCard[i];
+        }
+        for (int i = 0; i <= playerCardVal; i++) {
+            tempPlayerSum += playerCard[i];
+        }
+        
+        while (true) {
+            cout << "[+] Dealer hand: ";
+            for (int i = 0; i <= dealerCardVal; i++) {
+                cout << dealerCard[i] << " ";
+            }
+            cout << endl;
+            
+            cout << "[+] Player hand: ";
+            for (int i = 0; i <= playerCardVal; i++) {
+                cout << playerCard[i] << " ";
+            }
+            cout << endl;
+            
+            cout << "[?] Player sum: " << tempPlayerSum << endl;
+            
+            if (tempPlayerSum > 21) {
+				std::this_thread::sleep_for(std::chrono::milliseconds(500));
+                cout << "[-] You busted! You lost!" << endl;
+                break;
+            }
+            
+            if (tempPlayerSum == 21) {
+				std::this_thread::sleep_for(std::chrono::milliseconds(500));
+                cout << "[777] Blackjack!" << endl;
+                if (tempDealerSum == 21) {
+					std::this_thread::sleep_for(std::chrono::milliseconds(500));
+                    cout << "Push! Tie game." << endl;
+                } else {
+					std::this_thread::sleep_for(std::chrono::milliseconds(500));
+                    cout << "[+] You won!" << endl;
+                }
+                break;
+            }
+            
+            cout << "[?] Enter your next move (Hit - H, Stand - S, End the game - X): ";
+            cin >> choice;
+            
+            if (choice == 'H' || choice == 'h') {
+                playerCardVal++;
+                if (playerCardVal < POSSIBLECARDS) {
+                    tempPlayerSum += playerCard[playerCardVal];
+                }
+            }
+            else if (choice == 'S' || choice == 's') {
+                cout << "[+] Dealer's turn..." << endl;
+				std::this_thread::sleep_for(std::chrono::milliseconds(500));
+                for (int i = 0; i <= dealerCardVal; i++) {
+                    cout << dealerCard[i] << " ";
+                }
+				std::this_thread::sleep_for(std::chrono::seconds(2));
+                cout << endl;
+                
+                while (tempDealerSum < 17) {
+                    dealerCardVal++;
+                    if (dealerCardVal < POSSIBLECARDS) {
+                        tempDealerSum += dealerCard[dealerCardVal];
+                        cout << "[+] Dealer draws: " << dealerCard[dealerCardVal] 
+                             << " | Total: " << tempDealerSum << endl;
+							 std::this_thread::sleep_for(std::chrono::seconds(1));
+                    }
+                }
+                
+                if (tempDealerSum > 21) {
+					std::this_thread::sleep_for(std::chrono::milliseconds(500));
+                    cout << "[+] Dealer busted! You won!" << endl;
+                }
+                else if (tempDealerSum > tempPlayerSum) {
+					std::this_thread::sleep_for(std::chrono::milliseconds(500));
+                    cout << "[-] Dealer wins with " << tempDealerSum << " vs your " << tempPlayerSum << endl;
+                }
+                else if (tempPlayerSum > tempDealerSum) {
+					std::this_thread::sleep_for(std::chrono::milliseconds(500));
+                    cout << "[+] You win with " << tempPlayerSum << " vs dealer's " << tempDealerSum << endl;
+                }
+                else {
+					std::this_thread::sleep_for(std::chrono::milliseconds(500));
+                    cout << "[=] Push! It's a tie." << endl;
+                }
+                break;
+            }
+            else if (choice == 'X' || choice == 'x') {
+                cout << "[+] Game ended." << endl;
+                cin.ignore();
+                cin.get();
+                return 0;
+            }
+            
+            cout << endl;
+        }
+        
+        cout << endl << "[?] Play again? (y/n): ";
+        cin >> choice;
+        if (choice == 'N' || choice == 'n') {
+            break;
+        }
+        
+        fillArray(dealerCard, playerCard, POSSIBLECARDS);
+    }
+    return 0;
+}
